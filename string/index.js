f583081e50b5b78165daf3a7009cb123ffead3b5
艾因 <lerrannpasar@gmail.com>
2025-02-26 23:21:14 -0500

    put things together; ask questions to help youself understand the problem

--- string/index.js
+++ string/index.js
@@ -1,128 +1,134 @@
-function test() {
-  console.log('test');
-}
+// 1768. Merge Strings Alternately
+// You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
 
-test();
+// Return the merged string.
 
-//live coding
-//Q1
-//   Find the First Repeating Character
-// Write a function firstRepeatingChar that takes a string and returns the first character that appears more than once.
-//  If there are no repeating characters, return null.
-function firstRepeatingChar(str) {
-  for (let index = 0; index < str.length - 1; index++) {
-    const ch = str[index];
-    if (str.substring(index + 1).indexOf(ch) !== -1) return ch;
+// Example 1:
+
+// Input: word1 = "abc", word2 = "pqr"
+// Output: "apbqcr"
+// Explanation: The merged string will be merged as so:
+// word1:  a   b   c
+// word2:    p   q   r
+// merged: a p b q c r
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+  const minLength = Math.min(word1.length, word2.length);
+  console.log(minLength);
+
+  let resultStr = '';
+  const resultStrArr = [];
+  resultStrArr.push([1, 2, 3]);
+  resultStrArr.push(Array.from('456'));
+  resultStrArr.push([7, 8, [9, 10]]);
+  console.log('new array: ', resultStrArr); // new array:  (3) [Array(3), Array(3), Array(3)]
+  // Array.toString() method 会把多层的Array 都铺平显示， 即多维数组也能输出为一维的字符串
+  console.log('new array string: ', resultStrArr.toString()); // new array string:  1,2,3,4,5,6,7,8,9,10
+  // Array.join(separator) method 不会 把多层的Array 铺平展示，所以多维数组的展示会有问题
+  console.log('new array string join: ', resultStrArr.join('')); // new array string join:  1,2,34,5,67,8,9,10
+  console.log(
+    'new array string -> array: ',
+    Array.from(resultStrArr.toString())
+  );
+
+  for (let i = 0; i < minLength; i++) {
+    resultStr = resultStr.concat(word1.charAt(i), word2.charAt(i));
+    // resultStr + word1.charAt(i) + word2.charAt(i);
+  }
+  console.log('resultStr: ', resultStr);
+
+  word1.length > minLength &&
+    (resultStr = resultStr.concat(word1.slice(minLength)));
+  word2.length > minLength &&
+    (resultStr = resultStr.concat(word2.slice(minLength)));
+
+  //test if string.forEach() is a function?  A: No, it's not a function.
+  // word1.slice(0).forEach((ch) => {
+  //   console.log('forEach of string: ' + ch);
+  // });
+  for (const ch of word1.slice(0)) {
+    console.log(ch);
   }
 
-  return null;
-}
-
-console.log('duplicated char: ', firstRepeatingChar('abca'));
-
-// Test cases
-// console.log(firstRepeatingChar('abca')); // → 'a'
-console.log(firstRepeatingChar('abcdef')); // → null
-console.log(firstRepeatingChar('aabbcc')); // → 'a'
-console.log(firstRepeatingChar('hwertyfgh')); // → 'h'
-
-//Q2 :
-/*
-Find the Most Frequent Element
-Write a function mostFrequent that finds the most frequently occurring element in an array.
-*/
-
-const mostFrequent = (array) => {
-  const chCountMap = new Map();
-  for (let index = 0; index < array.length; index++) {
-    const ch = array[index];
-    if (chCountMap.has(ch)) {
-      chCountMap.set(ch, chCountMap.get(ch) + 1);
-    } else chCountMap.set(ch, 1);
-  }
-
-  console.log(chCountMap);
-  let maxCount = 0;
-  let mostFreCh;
-  chCountMap.forEach((count, ch, map) => {
-    if (count > maxCount) {
-      maxCount = count;
-      mostFreCh = ch;
-    }
-  });
-
-  return mostFreCh;
+  return resultStr;
 };
 
-console.log('mostFrequent:');
-console.log(mostFrequent([1, 2, 3, 3, 2, 3])); // → 3
-console.log(mostFrequent(['a', 'b', 'a', 'a', 'c'])); // → 'a'
+console.log(mergeAlternately('ab', 'pqrs'));
 
-//Q3
-// Write a function called commonElements that takes in any number of arrays in the
-// argument. The arrays may contain both numbers and strings. It should return a new array
-// with all the common elements (both numbers and/or strings) from the given input.
-// If there are no common numbers/strings, return "Nothing in Common!"
-// Assume there are no duplicates within the array.
+// 1071. Greatest Common Divisor of Strings
+// For two strings s and t, we say "t divides s"
+//    if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
 
-//make a set and return that
-// the function should return
+// Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
 
-console.log('Q3:');
-// 使用 Object
-let obj = {};
-obj['key1'] = 'value1';
-obj[2] = 'value2';
-console.log(obj); // 输出: { '2': 'value2', key1: 'value1' }
+// Example 1:
 
-// 使用 Map
-let map = new Map();
-map.set('key1', 'value1');
-map.set(2, 'value2');
-console.log(map); // 输出: Map { 'key1' => 'value1', 2 => 'value2' }
+// Input: str1 = "ABCABC", str2 = "ABC"
+// Output: "ABC"
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+  let shortStr = str1.length < str2.length ? str1 : str2;
+  // console.log(shortStr);
 
-function commonElements(...arrays) {
-  // F
-  // for()
-  //would be slow but one solution would just to have nested for loops and test
+  let shortStr2 = 'leet'.length < 'code'.length ? 'leet' : 'code';
+  // console.log('2: ', shortStr2);
+  // if (str1.indexOf(shortStr) !== -1 && str2.indexOf(shortStr) !== -1)
+  //   return shortStr;
+  // console.log('7/2 = ', 7 / 2);
 
-  if (arrays.length === 0) return 'Nothing in Common!';
-  else if (arrays.length === 1) return arrays[1];
+  // If the shortest "str" is not "gcdStr," then proceed with
+  // a step-by-step search (starting from the largest and working downward).
 
-  // arrays contains more than one array
-  const firstArr = arrays[0];
-  const comElements = [];
+  //What are the possible candidate strings?
 
-  // go through every element in firstArr,
-  // to check if this element is in every other array in the arrays parameter
-  firstArr.forEach((element) => {
-    let i = 1;
-    for (; i < arrays.length; i++) {
-      const arr = arrays[i];
-      if (arr.indexOf(element) === -1) break; // TODO 验证 前面的数组都包含的元素，最后一个数组 不包含
+  // Here we make use of prefix strings. If a string base is the GCD string,
+  // it must be a prefix of both str1 and str2.
+  // So instead of trying every combination of characters,
+  // we instead just take each prefix string of str1 (or str2)
+  // and check if it is the GCD string.
+
+  // Above is the reason why I needn't have a for loop for i !!!
+  // for (let i = 0; i < shortStr.length; i++) {
+  for (let j = shortStr.length; j >= 1; j--) {
+    const str = shortStr.slice(0, j);
+    // if (str1.indexOf(str) !== -1 && str2.indexOf(str) !== -1) return str;
+
+    let cpyStr1 = str1.slice(0);
+    let cpyStr2 = str2.slice(0);
+    const strLen = str.length;
+
+    while (cpyStr1.length >= strLen) {
+      if (cpyStr1.indexOf(str) !== 0) {
+        break;
+      } else {
+        // If the target string starts with str, remove this str from the target string.
+        cpyStr1 = cpyStr1.slice(strLen);
+      }
+    }
+    while (cpyStr2.length >= strLen) {
+      if (cpyStr2.indexOf(str) !== 0) {
+        break;
+      } else {
+        // If the target string starts with str, remove this str from the target string.
+        cpyStr2 = cpyStr2.slice(strLen);
+      }
     }
 
-    if (i === arrays.length) comElements.push(element);
+    if (cpyStr1.length === 0 && cpyStr2.length === 0) return str;
+  }
+  // }
 
-    // comElements.push('MOnday');
-  });
+  return '';
+};
 
-  // console.log('comElements : ' + comElements);
-
-  return comElements.length > 0 ? comElements : 'Nothing in Common!';
-}
-
-const arr1 = [2, 10, 'cat', 3, 99, 2000, 'dog', 'lion'];
-const arr2 = [3, 7, 2, 2000, 1, 'dog', 'cat'];
-const arr3 = [2, 100, 2000, 'dog', 3, 'lion'];
-
-const arr4 = [2, 100, 2000, 'dog', 3, 'lion'];
-const arr5 = ['lion', 'tiger', 2, 3];
-const arr6 = ['giray', 'monkey', 2, 3];
-const arr7 = ['arr', 3];
-
-console.log('test 1: ');
-console.log(commonElements(arr1, arr2, arr3)); //-> [2, 3, 2000, 'dog']
-console.log(commonElements(arr4, arr5, arr6, arr7)); //-> [2, 3, 2000, 'dog']
-
-console.log('End of Q3');
+console.log(gcdOfStrings('ABABAB', 'ABAB'));
+console.log(gcdOfStrings('ABCABC', 'ABC'));
+console.log(gcdOfStrings('LEET', 'CODE'));
