9b10c10bfe107995782f2f10e3391e47c88ea0b7
艾因 <lerrannpasar@gmail.com>
2025-11-05 10:50:20 -0500

    updates

---
+++ sliding-window/index_#219_todo_.js
@@ -0,0 +1,18 @@
+// 219. Contains Duplicate II
+
+// Example 1:
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {};
