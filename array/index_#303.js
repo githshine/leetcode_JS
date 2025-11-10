5fb4c7f273cca903e72b4417ab0e521050404b6d
艾因 <lerrannpasar@gmail.com>
2025-11-10 07:43:36 -0500

    one more

---
+++ array/index_#303.js
@@ -0,0 +1,49 @@
+// 303. Range Sum Query - Immutable
+
+// Example 1:
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
+
+// Explanation
+// NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  this.array = nums; // 保存存入的数组 到实例属性中
+};
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  const arr = this.array; // 拿到实例保存的数组
+
+  let sum = 0;
+  for (let i = left; i <= right; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+const numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+console.log(numArray.sumRange(0, 2));
+console.log(numArray.sumRange(2, 5));
+console.log(numArray.sumRange(0, 5));
+// numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
--- index.js
+++
@@ -1,1 +0,0 @@
-// 303. Range Sum Query - Immutable
