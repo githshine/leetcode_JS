14b092cfa13cab8de5747073b88eeb918fc306c7
艾因 <lerrannpasar@gmail.com>
2025-11-20 10:33:03 -0500

    medium problem seems more complicated because it may include other helper functions. The core is to break one big question into small pieces.

--- two-pointer/index_#05.js
+++ two-pointer/index_#05.js
@@ -13,36 +13,6 @@
  * @param {string} s
  * @return {string}
  */
-// time out !!
-var longestPalindrome_01 = function (s) {
-  function isPalindrome(arr) {
-    // let tmp = [...arr];
-    return arr.join('') === [...arr].reverse().join('');
-  }
-
-  let i = 0,
-    j = 0;
-  let maxLen = 0;
-  let longestPalindrm = '';
-  let strArr = [];
-  while (i < s.length) {
-    strArr.push(s[j]);
-    if (isPalindrome(strArr) && j - i + 1 > maxLen) {
-      maxLen = j - i + 1;
-      longestPalindrm = strArr.join('');
-    }
-
-    if (j < s.length - 1) {
-      j++;
-    } else {
-      // j >= s.length
-      strArr = [];
-      i++;
-      j = i;
-    }
-  }
-  return longestPalindrm;
-};
 
 // 思路要点：
 
